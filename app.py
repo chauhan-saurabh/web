950984fe76f26554b780813c7739f218013b38bd
Saurabh Chauhan <Saurabh Chauhan>
2023-11-04 03:07:51 -0700

    code removed

--- app.py
+++ app.py
@@ -1,119 +1,125 @@
-from flask import Flask, request, jsonify
 from splinter import Browser
 import time
-from selenium import webdriver  
-from selenium.webdriver.common.keys import Keys  
-from selenium.webdriver.chrome.options import Options
+
+# Initialize a Splinter browser instance with the Chrome WebView option
+browser = Browser('chrome', headless=False, incognito=True)
+
+# Define the URL you want to open in the WebView
+url = 'https://google.com'
+
+# Open the URL in the WebView
+browser.visit(url)
+
+# You can interact with the WebView here, e.g., click buttons, fill forms, etc.
+time.sleep(10)
+# When you're done, you can close the browser
+browser.quit()
 
 
-app = Flask(__name__)
+
+# from flask import Flask, request, jsonify
+# from splinter import Browser
+# import time
+# from selenium import webdriver  
+# from selenium.webdriver.common.keys import Keys  
+# from selenium.webdriver.chrome.options import Options
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# app = Flask(__name__)
 
-@app.route('/execute', methods=['GET'])
-def execute_code():
-    try:
-        # browser = Browser('chrome')
-        # browser = Browser('chrome', headless=True)
-        options = webdriver.ChromeOptions()
-        options.add_argument('--no-sandbox')
-        # options.add_argument('--headless')
-        options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--disable-extensions')
-        options.add_argument('--disable-gpu')
-        driver = webdriver.Chrome(options=options)
-        driver.set_window_size(1200, 600)
 
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+# @app.route('/execute', methods=['GET'])
+# def execute_code():
+#     try:
+#         browser = Browser('chrome')
+#         browser.visit('https://vip.theralytics.net/')
         
-        browser = Browser('chrome', options=options)
-        browser.visit('https://vip.theralytics.net/')
-        
-        time.sleep(1)
+#         time.sleep(1)
 
-        browser.fill("userName", 'vipadmin')
+#         browser.fill("userName", 'vipadmin')
 
-        password_field = browser.find_by_xpath('//input[@type="password"]')
-        password_field.fill("TheraAdmin2020@2")
+#         password_field = browser.find_by_xpath('//input[@type="password"]')
+#         password_field.fill("TheraAdmin2020@2")
 
-        login_button = browser.find_by_css("button")
-        if login_button:
-            login_button.click()
-        else:
-            return jsonify({"error": "Login button not found!"})
+#         login_button = browser.find_by_css("button")
+#         if login_button:
+#             login_button.click()
+#         else:
+#             return jsonify({"error": "Login button not found!"})
 
-        navBar = browser.find_by_css(".nav-menu")
-        if navBar:
-            navBar.click()
-        else:
-            return jsonify({"error": "navBar with class name not found!"})
+#         navBar = browser.find_by_css(".nav-menu")
+#         if navBar:
+#             navBar.click()
+#         else:
+#             return jsonify({"error": "navBar with class name not found!"})
 
-        client = browser.find_by_text("Manage Client")
-        if client:
-            client.first.click()
-        else:
-            return jsonify({"error": "Div element with text 'Client' not found!"})
+#         client = browser.find_by_text("Manage Client")
+#         if client:
+#             client.first.click()
+#         else:
+#             return jsonify({"error": "Div element with text 'Client' not found!"})
 
-        client_list = browser.find_by_text("Client List")
-        if client_list:
-            client_list.first.click()
-        else:
-            return jsonify({"error": "Div element with text 'Client List' not found!"})
+#         client_list = browser.find_by_text("Client List")
+#         if client_list:
+#             client_list.first.click()
+#         else:
+#             return jsonify({"error": "Div element with text 'Client List' not found!"})
 
-        client_search = browser.find_by_name('Name')
-        if client_search:
-            client_search.fill('sean')
-        else:
-            return jsonify({"error": "client_search not found!"})
+#         client_search = browser.find_by_name('Name')
+#         if client_search:
+#             client_search.fill('sean')
+#         else:
+#             return jsonify({"error": "client_search not found!"})
 
-        time.sleep(3)
+#         time.sleep(3)
 
-        yellow_eye = browser.find_by_css(".tbactionbtn.yellowfont")
-        if yellow_eye:
-            yellow_eye.click()
-        else:
-            return jsonify({"error": "yellow_eye with class name not found!"})
+#         yellow_eye = browser.find_by_css(".tbactionbtn.yellowfont")
+#         if yellow_eye:
+#             yellow_eye.click()
+#         else:
+#             return jsonify({"error": "yellow_eye with class name not found!"})
 
-        time.sleep(3)
+#         time.sleep(3)
 
-        try:
-            edit_button = browser.find_by_css(".smbtn.editclient")
-            edit_button.click()
-        except StaleElementReferenceException:
-            edit_button = browser.find_by_css(".smbtn.editclient")
-            edit_button.click()
+#         try:
+#             edit_button = browser.find_by_css(".smbtn.editclient")
+#             edit_button.click()
+#         except StaleElementReferenceException:
+#             edit_button = browser.find_by_css(".smbtn.editclient")
+#             edit_button.click()
 
-        time.sleep(1)
+#         time.sleep(1)
 
-        try:
-            middleNameField = browser.find_by_name("middleName")
-            middleNameField.fill('Davidson')
-        except StaleElementReferenceException:
-            middleNameField = browser.find_by_name("middleName")
-            middleNameField.fill('Davidson')
-            time.sleep(1)
+#         try:
+#             middleNameField = browser.find_by_name("middleName")
+#             middleNameField.fill('Davidson')
+#         except StaleElementReferenceException:
+#             middleNameField = browser.find_by_name("middleName")
+#             middleNameField.fill('Davidson')
+#             time.sleep(1)
 
-        try:
-            submitButton = browser.find_by_text("Save & Continue")
-            submitButton.click()
-        except StaleElementReferenceException:
-            submitButton = browser.find_by_text("Save & Continue")
-            submitButton.click()
+#         try:
+#             submitButton = browser.find_by_text("Save & Continue")
+#             submitButton.click()
+#         except StaleElementReferenceException:
+#             submitButton = browser.find_by_text("Save & Continue")
+#             submitButton.click()
 
-        time.sleep(10)
+#         time.sleep(10)
 
-        browser.quit()
+#         browser.quit()
 
-        return jsonify({"message": "Code executed successfully"})
-    except Exception as e:
-        return jsonify({"error": str(e)})
+#         return jsonify({"message": "Code executed successfully"})
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8083)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8082)
 
 
 
